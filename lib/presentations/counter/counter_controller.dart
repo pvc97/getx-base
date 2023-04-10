fefe18102c7276d5ed1e829c0fdd51864eaddfbb
pvc97 <cuongpv2209@gmail.com>
2023-04-10 13:36:54 +0700

    Interact between counter controller with home controller.

--- lib/presentations/counter/counter_controller.dart
+++ lib/presentations/counter/counter_controller.dart
@@ -1,15 +1,16 @@
+import 'package:demo_getx/presentations/home/home_controller.dart';
+import 'package:demo_getx/utils/extensions/bg_color_ext.dart';
 import 'package:get/get.dart';
 
 class CounterController extends GetxController {
-  // The counter value
+  final homeController = Get.find<HomeController>();
+
   final _counter = 0.obs;
 
-  // The counter value
   int get counter => _counter.value;
 
-  // Increment the counter
-  void increment() => _counter.value++;
-
-  // Decrement the counter
-  void decrement() => _counter.value--;
+  void increment() {
+    final step = homeController.color.value.stepCount;
+    _counter.value += step;
+  }
 }
