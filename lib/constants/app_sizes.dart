f699a7883139d507942c7cb2eca3c2192039f28c
pvc97 <cuongpv2209@gmail.com>
2023-04-13 22:44:39 +0700

    Add const.

---
+++ lib/constants/app_sizes.dart
@@ -0,0 +1,26 @@
+/// Constant sizes to be used in the app (paddings, gaps, rounded corners etc.)
+class Sizes {
+  Sizes._();
+  static const s1 = 1.0;
+  static const s2 = 2.0;
+  static const s3 = 3.0;
+  static const s4 = 4.0;
+  static const s8 = 8.0;
+  static const s12 = 12.0;
+  static const s16 = 16.0;
+  static const s20 = 20.0;
+  static const s24 = 24.0;
+  static const s32 = 32.0;
+  static const s44 = 44.0;
+  static const s48 = 48.0;
+  static const s56 = 56.0;
+  static const s60 = 60.0;
+  static const s64 = 64.0;
+  static const s108 = 108.0;
+  static const s112 = 112.0;
+  static const s120 = 120.0;
+  static const s200 = 200.0;
+  static const s300 = 300.0;
+  static const s400 = 400.0;
+  static const s600 = 600.0;
+}
